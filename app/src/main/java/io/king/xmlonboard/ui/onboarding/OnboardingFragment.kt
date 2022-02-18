77a58b408bf2fb39d3b87be1362c62f95a69e68f
andymaking <andymaking@gmail.com>
2022-02-18 19:18:01 +0100

    Update

--- app/src/main/java/io/king/xmlonboard/ui/onboarding/OnboardingFragment.kt
+++ app/src/main/java/io/king/xmlonboard/ui/onboarding/OnboardingFragment.kt
@@ -1,5 +1,6 @@
 package io.king.xmlonboard.ui.onboarding
 
+import android.icu.text.CaseMap
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -11,6 +12,11 @@ import io.king.xmlonboard.databinding.FragmentLoginBinding
 import io.king.xmlonboard.databinding.FragmentOnboardingBinding
 
 class OnboardingFragment : Fragment() {
+
+    private var titleList = mutableListOf<String>()
+    private var descList = mutableListOf<String>()
+    private var picsList = mutableListOf<Int>()
+
     private var _binding: FragmentOnboardingBinding? = null
     private val binding get() = _binding!!
 
@@ -27,9 +33,12 @@ class OnboardingFragment : Fragment() {
         binding.logIn.setOnClickListener { view ->
             view.findNavController().navigate(OnboardingFragmentDirections.actionOnboardingFragmentToLoginFragment())
         }
-
-
-
         return binding.root
+    }
+
+    private fun addToList(title: String, description: String, picture: Int){
+        titleList.add(title)
+        descList.add(description)
+        picture.and(picture)
     }
 }
