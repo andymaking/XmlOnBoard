7421f2357d0b833065a8449b1e0fb336180deaee
andymaking <andymaking@gmail.com>
2022-02-22 11:27:15 +0100

    Update

--- app/src/main/java/io/king/xmlonboard/ui/login/LoginFragment.kt
+++ app/src/main/java/io/king/xmlonboard/ui/login/LoginFragment.kt
@@ -5,9 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import io.king.xmlonboard.R
 import io.king.xmlonboard.databinding.FragmentFrontBinding
 import io.king.xmlonboard.databinding.FragmentLoginBinding
+import kotlin.random.Random
 
 class LoginFragment : Fragment() {
     private var _binding: FragmentLoginBinding? = null
@@ -18,9 +20,13 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentLoginBinding.inflate(inflater, container, false)
+
+
+        binding.clicks.setOnClickListener{view ->
+            findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToSignupFragment())
+        }
+
+
         return binding.root
-
-
-
-    }
+        }
 }
